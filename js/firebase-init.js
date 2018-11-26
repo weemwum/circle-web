958a834c0a46f69e6a9f7d6991c842354b1afa57
weemwum <31605623+weemwum@users.noreply.github.com>
2018-11-25 20:48:23 -0600

    Add files via upload

---
+++ js/firebase-init.js
@@ -0,0 +1,8 @@
+var config = {
+  apiKey: "AIzaSyALIeMslA7TXSJs3zMjJdHZ90vFCwBEdMM",
+  authDomain: "circle-web.firebaseapp.com",
+  databaseURL: "https://circle-web.firebaseio.com",
+  projectId: "circle-web",
+  storageBucket: "circle-web.appspot.com",
+};
+firebase.initializeApp(config);
